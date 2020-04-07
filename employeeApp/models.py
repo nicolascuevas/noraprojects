b48ca10a23e324576383b815a485833800158682
nicolascuevas <nicolascuevas@gmail.com>
2020-04-07 18:07:11 -0400

    gitflow-feature-stash: import-employee-slack

--- employeeApp/models.py
+++ employeeApp/models.py
@@ -12,13 +12,16 @@ from datetime import datetime, time
 
 from noraprojects.task import send_slack_notification
 from meals import helpers
+import uuid
 
 # Create your models here.
 
 
 class Employee(models.Model):
-    identifier = models.CharField(max_length=10, null=True, blank=True)
+    identifier = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4().hex)
     email = models.EmailField(max_length=70, null=True, blank=True)
+    name = models.CharField(max_length=10, null=True, blank=True)
+    slack_id = models.CharField(max_length=10, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Employee'
