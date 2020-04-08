ff2fb2bcb5ea26ee814f8f3d98d901998b05228c
nicolascuevas <nicolascuevas@gmail.com>
2020-04-08 10:50:34 -0400

    fixes on uuid for employee

--- employeeApp/views.py
+++ employeeApp/views.py
@@ -32,8 +32,8 @@ def employee_meal_choose(request, user_uuid):
     options = Option.objects.filter(menu__id=current_menu.id)
 
     if request.method == "POST":
-        if not request.POST.get("option_id") or  not request.POST.get("employee_id") or  not request.POST.get("menu_id"):
-            return HttpResponse('Missing information', status=403)
+        if not request.POST.get("option_id") or  not request.POST.get("employee_id") or  not request.POST.get("menu_id") or current_menu.can_choose_menu == False:
+            return HttpResponse('Missing Information', status=403)
         choosen_option = options[0]
         if request.POST.get("option_id"):
             choosen_option = Option.objects.filter(id=request.POST.get("option_id"))[0]
@@ -41,7 +41,7 @@ def employee_meal_choose(request, user_uuid):
         customization = request.POST.get("customization")
         
 
-        if len( Order.objects.filter(employee_identifier=employee, menu__id=choosen_menu.id) ) == 0:
+        if len( Order.objects.filter(employee_identifier=user_uuid, menu__id=choosen_menu.id) ) == 0:
             order = Order(
                 menu=choosen_menu,
                 option=choosen_option,
@@ -50,7 +50,7 @@ def employee_meal_choose(request, user_uuid):
             )
             order.save()
         else:
-            order = Order.objects.filter(employee_identifier=employee, menu=choosen_menu)[0]
+            order = Order.objects.filter(employee_identifier=user_uuid, menu=choosen_menu)[0]
             order.customization = customization
             order.option = choosen_option
             order.save()
